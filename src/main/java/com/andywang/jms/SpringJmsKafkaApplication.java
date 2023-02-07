3696cff16ffd701549b3b45cb3da40ecbe9431d9
andy <andy.wang@webcomm.com.tw>
2023-02-07 10:09:16 +0800

    init

---
+++ src/main/java/com/andywang/jms/SpringJmsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.andywang.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJmsKafkaApplication.class, args);
+    }
+
+}
