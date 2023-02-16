a685e9f938b35d849891594be2e0ee46b1f6ab0a
andy <andy.wang@webcomm.com.tw>
2023-02-16 18:24:19 +0800

    kafkaListener with header and payload

--- src/main/java/com/andywang/jms/consumer/MessageReceiver.java
+++ src/main/java/com/andywang/jms/consumer/MessageReceiver.java
@@ -4,19 +4,22 @@ import com.andywang.jms.config.KafkaTopicConfig;
 import com.andywang.jms.producer.KafkaProducerDemo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class MessageReceiver {
 
-    @KafkaListener(id = "andy", topics = KafkaTopicConfig.INTEREST_TOPIC)
-    public void listen(String in) {
-        log.info("Message Received: " + in);
-    }
-
     @KafkaListener(id = "my", topics = KafkaProducerDemo.MY_TOPIC)
     public void listenMyTopic(String in) {
         log.info("Message Received: " + in);
+    }
+
+    @KafkaListener(id = "andy", topics = KafkaTopicConfig.INTEREST_TOPIC)
+    public void listenWithHeaders(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        System.out.println("Received Message: " + message + " from partition: " + partition);
     }
 }
