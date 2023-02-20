00d3d5eb111ce755350e0b08a9bba6d253c75875
andy <andy.wang@webcomm.com.tw>
2023-02-20 11:16:38 +0800

    add constructor

--- src/main/java/com/andywang/jms/multitype/TypeTwo.java
+++ src/main/java/com/andywang/jms/multitype/TypeTwo.java
@@ -1,12 +1,16 @@
 package com.andywang.jms.multitype;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TypeTwo {
 
     private String message;
